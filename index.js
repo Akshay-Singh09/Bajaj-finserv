f759710cd26d60dc09cc713b2393c4b890c9ad4f
AKSHAY PRATAP SINGH <77232216+Akshay-Singh09@users.noreply.github.com>
2022-07-11 14:19:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Server On");
+});
+
 app.post('/', (request, response) => {
     var arr =request.query.data;
     var len = arr.length;
